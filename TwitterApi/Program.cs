6a72a7992c61f567c82689b3b4180617e7d024c1
matthew-potts <matthew-potts@live.com>
2022-06-22 15:23:37 +0100

    Added Authorisation

--- TwitterApi/Program.cs
+++ TwitterApi/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using CoreTweet;
+using Authorisation;
 
+namespace TwitterAPI
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var details = GetAuthorised();
+            var session = OAuth.Authorize(details.customer_key, details.customer_secret);
+
+            Console.ReadLine();
+        }
+
+        public static Authorisation.Authorisation GetAuthorised()
+        {
+            return new Authorisation.Authorisation
+            {
+                customer_key = "58CjOPVH8fq2KanmdaPwCgWoy",
+                customer_secret = "rzNrqz7mZLBzguvGIej0OWqgCc1VTP14yeuxUT5kkHE15R9pJO"
+            };
+        }
+
+    }
+}
